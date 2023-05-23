7f3435f3558d8feeb5ea7aacfd734adcb2960abd
Josefinahelou <josefina.helou@outlook.com>
2023-05-23 20:21:11 -0300

    se commitean ej

---
+++ src/Ejercicios1/Foriterator.java
@@ -0,0 +1,10 @@
+package Ejercicios1;
+
+public class Foriterator {
+    public static void main(String[] args) {
+        for (int contador = 0; contador <= 5; contador++) {
+            System.out.println("contador :[" + contador + "]");
+        }
+
+    }
+}
